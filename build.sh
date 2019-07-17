ea0af9461b8ad5c8cf2539aaa61035b3bb97021f
Darryl T. Agostinelli <dagostinelli@gmail.com>
2019-07-16 23:04:40 -0500

    fixed it!

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-# pandoc -s essay.md -o essay.pdf --template=mla-template.tex --filter pandoc-citeproc
-# pandoc -s essay.md -o essay.pdf --filter pandoc-citeproc -M reference-section-title="Works Cited" --csl=modern-language-association.csl
-# (cat essay.md & cat mla-suffix.tex) | pandoc -o essay.pdf --template=mla-template.tex --filter pandoc-citeproc
-#pandoc -A mla-suffix.tex -s essay.md -o essay.pdf --template=mla-template.tex --filter pandoc-citeproc
-(cat essay.md & cat mla-suffix.tex) | pandoc -o essay.docx --template=mla-template.tex --filter pandoc-citeproc
+#! /bin/bash
+
+cat essay.md mla-suffix.tex | pandoc -o essay.pdf --template=mla-template.tex --filter pandoc-citeproc
